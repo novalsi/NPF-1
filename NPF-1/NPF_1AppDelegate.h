1baceef7a306805f4648a8606f55202964b5c7b8
Student <student@sl2670-m05.infotech.it.rit.edu>
2013-09-10 15:08:03 -0400

    Initial Commit

---
+++ NPF-1/NPF_1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NPF_1AppDelegate.h
+//  NPF-1
+//
+//  Created by Student on 9/10/13.
+//  Copyright (c) 2013 Nick Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NPF_1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NPF-1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NPF-1
+//
+//  Created by Student on 9/10/13.
+//  Copyright (c) 2013 Nick Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NPF_1AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NPF_1AppDelegate class]));
+    }
+}
